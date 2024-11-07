08a454e813bd26583bb4ff00bc2d4e97b8d4b344
patne <negripatr@gmail.com>
2024-11-07 09:12:22 +0100

    feat: Kartenerstellung inkl. Unit-Tests

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,21 @@
+package Main;
+
+import Controller.CardCreatorController;
+import View.CardCreatorView;
+
+import javax.swing.*;
+
 public class Main {
-    public static void main(String [] args) {
-        System.out.println("Hallo Flash Projektmitglieder!");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Initialisierung der CardCreatorView und des zugehörigen Controllers
+            CardCreatorView cardCreatorView = new CardCreatorView();
+            if (cardCreatorView == null) {
+                System.out.println("CardCreatorView ist null!");
+            } else {
+                System.out.println("CardCreatorView erfolgreich initialisiert.");
+                new CardCreatorController(cardCreatorView);
+            }
+        });
     }
 }
