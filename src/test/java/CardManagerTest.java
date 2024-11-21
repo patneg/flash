24b1f15fb30c119fb420282274054949bd8748c4
Christ Solèr <Christ.Soler@mt.com>
2024-11-21 09:21:00 +0100

    Fixed JUnit5 Support in POM and Library

---
+++ src/test/java/CardManagerTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+class CardManagerTest {
+
+    @org.junit.jupiter.api.Test
+    void loadCardsFromFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveCardsToFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFlashCards() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addFlashCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteFlashCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateFlashCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+}
